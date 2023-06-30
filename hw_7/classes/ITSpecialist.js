3aa87d3401aa9ba8560fda6130f1fa0ecd24c47a
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-07-01 01:07:25 +0300

    task 2 - 4 (html)

---
+++ hw_7/classes/ITSpecialist.js
@@ -0,0 +1,24 @@
+class ITSpecialist {
+    constructor(name, grade, experience_in_years, age, country, salary) {
+        this.name = name;
+        this.grade = grade;
+        this.experience_in_years = experience_in_years;
+        this.age = age;
+        this.country = country;
+        this._salary = salary;
+    }
+
+    getAllInfo() {
+        return `Name: ${this.name}, Grade: ${this.grade}, Experience: ${this.experience_in_years} years, Age: ${this.age}, Country: ${this.country}`;
+    }
+
+    setCountry(country) {
+        this.country = country;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+}
+
+module.exports = ITSpecialist;
---
+++ hw_7/classes/QA.mjs
@@ -0,0 +1,24 @@
+import { ITSpecialist } from './classes.fabric.js';
+
+export class QA extends ITSpecialist {
+    constructor(name, grade, experience_in_years, age, country, salary, isAqa) {
+        super(name, grade, experience_in_years, age, country, salary);
+        this.isAqa = isAqa;
+    }
+}
+// const newQA = new QA('Vova', 'Junior', 2, 22, 'Ukraine', 2500, false);
+//
+// const qaInfoDiv = document.getElementById('qaInfo');
+//
+// qaInfoDiv.innerHTML += `
+//     <h1>QA Information</h1>
+//     <p>Name: ${newQA.name()}</p>
+//     <p>Grade: ${newQA.grade()}</p>
+//     <p>Experience: ${newQA.experience_in_years()} years</p>
+//     <p>Age: ${newQA.age()}</p>
+//     <p>Country: ${newQA.country()}</p>
+//     <p>Salary: ${newQA.salary()}</p>
+//     <p>Is AQA: ${newQA.isAqa}</p>
+// `;
+
+module.exports = QA;
