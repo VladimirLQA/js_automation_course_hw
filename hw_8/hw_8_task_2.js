853e4b580bf30f8e7e48efba70f2e347e54fc69d
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-07-02 18:28:24 +0300

    task 2 - 2 done

--- hw_8/hw_8_task_2.js
+++ hw_8/hw_8_task_2.js
@@ -7,7 +7,7 @@
 
 2. Создайте функцию retryPromise, которая принимает функцию и максимальное количество повторных попыток в качестве входных данных.
    Функция должна выполнить предоставленную функцию и вернуть обещание. Если обещание отклонено, оно должно повторять выполнение функции до тех пор,
-   пока оно не будет зарезолвлено или не будет достигнуто максимальное количество попыток. Задержка каждой попытки на 1 секунду.
+   пока оно не будет зарезолвлено или не будет достигнуто   максимальное количество попыток. Задержка каждой попытки на 1 секунду.
    В качестве коллбэка напишите функцию getNumber, возвращаующую новый промис, в котором создается рандомное число от 1 до 10.
    Если число больше 5 - промис резолвается, если меньше либо равно - реджектается.
    Пример в консоли при отработке функции до Success с 3мя ретраями:
@@ -25,9 +25,6 @@
 
 {
     // #1
-    // const resolvePromise = () => new Promise((resolve) => resolve)
-
-
     const promiseSumOfTwo = (a, b) => {
         if (isNaN(a) || isNaN(b) || typeof a !== "number" || typeof b !== "number") {
             return Promise.reject(new Error(`Passed argument not a number`));
@@ -46,5 +43,67 @@
 
     promiseSumOfTwo(1)
         .then((result => console.log(result)))
-        .catch(error => throw new Error(error));
+        .catch(error => console.log(error));
+}
+
+{
+    // #2
+    {
+        const getNumber = () => new Promise((resolve, reject) => {
+            let randomNumber = Math.floor(Math.random() * 10) + 1;
+            if (randomNumber > 5) resolve(randomNumber);
+            else reject(new Error(`Number is less than or equal to 5`));
+        });
+
+        const retryPromise = (fn, maxRetries) => {
+            return new Promise((resolve, reject) => {
+                const retry = (attempt) => {
+                    fn().then(resolve)
+                        .catch(error => {
+                            if (attempt < maxRetries) {
+                                console.log(`Retrying ${attempt} time`);
+                                setTimeout(() => retry(attempt + 1), 1000);
+                            } else reject(error);
+
+                        });
+                };
+
+                console.log("First attempt");
+                retry(1);
+            });
+        }
+
+        retryPromise(getNumber, 5)
+            .then(result => console.log("Success", result))
+            .catch(error => console.error(error));
+    }
+
+    {
+        const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+        const getNumber = () => new Promise((resolve, reject) => {
+                const randomValue = Math.floor(Math.random() * 10) + 1;
+                if (randomValue > 5) resolve(randomValue);
+                else reject(new Error("Number is less than or equal to 5"));
+            });
+
+        const retryPromise = async (callback, maxRetries) => {
+            let retries = 0;
+            while (retries < maxRetries) {
+                try {
+                    return await callback();
+                } catch (error) {
+                    retries++;
+                    console.log(`Retrying ${retries} time`);
+                    await delay(1000);
+                }
+            }
+            throw new Error("Max retries reached");
+        }
+
+        console.log("First attempt");
+        retryPromise(getNumber, 5)
+            .then(result => console.log("Success", result))
+            .catch(error => console.error(error));
+    }
 }
